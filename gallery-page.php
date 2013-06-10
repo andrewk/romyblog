8ace4d9360aca9360b63257b50b61319f8a2d3d2
Andrew K <andrew@leftjustified.net>
2013-06-10 13:07:40 +1000

    gallery template and nav styles

---
+++ gallery-page.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Template Name: Gallery
+ * */
+
+get_header(); ?>
+
+<!-- Row for main content area -->
+	<section id="main" role="main">
+
+	<?php /* Start loop */ ?>
+	<?php while (have_posts()) : the_post(); ?>
+		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+			<header>
+				<h1 class="entry-title"><?php the_title(); ?></h1>
+			</header>
+			<div class="entry-content">
+				<?php the_content(); ?>
+			</div>
+		</article>
+	<?php endwhile; // End the loop ?>
+
+	</section>
+	<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
