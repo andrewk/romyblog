714f6fa003833e3528d5f20c4d92f6325e603ce5
Andrew K <andrew@leftjustified.net>
2013-06-15 17:52:55 +1000

    navigation

--- header.php
+++ header.php
@@ -2,41 +2,17 @@
 <!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
 <!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
 <!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
-<!-- Consider adding an manifest.appcache: h5bp.com/d/Offline -->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 
 	<title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
 
-	<!-- Mobile viewport optimized: j.mp/bplateviewport -->
 	<meta name="viewport" content="width=device-width" />
 
 	<!-- Favicon and Feed -->
 	<link rel="shortcut icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
 	<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> Feed" href="<?php echo home_url(); ?>/feed/">
-
-	<script src="<?php echo get_template_directory_uri(); ?>"></script>
-
-	<!--  iPhone Web App Home Screen Icon 
-	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-icon-ipad.png" />
-	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-icon-retina.png" />
-	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-icon.png" />
--->
-
-	<!-- Enable Startup Image for iOS Home Screen Web App 
-	<meta name="apple-mobile-web-app-capable" content="yes" />
-	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/mobile-load.png" />
-  -->
-	<!-- Startup Image iPad Landscape (748x1024) 
-	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-load-ipad-landscape.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape)" />
-	-->
-	<!-- Startup Image iPad Portrait (768x1004)
-	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-load-ipad-portrait.png" media="screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait)" />
-	-->
-	<!-- Startup Image iPhone (320x460)
-	<link rel="apple-touch-startup-image" href="<?php echo get_template_directory_uri(); ?>/img/devices/reverie-load.png" media="screen and (max-device-width: 320px)" />
-	-->
 <?php wp_head(); ?>
 
 </head>
@@ -48,5 +24,13 @@
 <img src="<?php echo get_template_directory_uri(); ?>/img/header.jpg">
 			</a></h1>
 </header>
+
+<nav role="navigation">
+	<a href="<?php echo home_url(); ?>">home</a>
+	<a href="<?php echo home_url(); ?>/drawings">drawings</a>
+	<a href="<?php echo home_url(); ?>/paintings">paintings</a>
+	<a href="<?php echo home_url(); ?>/writing">writing</a>
+	<a href="<?php echo home_url(); ?>/about">about</a>
+</nav>
 
 <section class="container row" role="document">
